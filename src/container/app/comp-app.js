301ddb030660e7dcfa73def4736cd1cde3008d91
singhplusplus <singhplusplus@gmail.com>
2021-05-05 09:59:38 +0530

    Created tw ui for buttons

--- src/container/app/comp-app.js
+++ src/container/app/comp-app.js
@@ -1,7 +1,15 @@
 import React from 'react';
+// import { render } from 'react-dom';
+import {ButtonLg} from './shared/button/comp-button'
 
 function App() {
-  return <h1 className="bg-red-300">App</h1>;
+  const buttongroup = ['First', 'Second', 'Third', 'Fourth'];
+  return ( 
+      <div className="space-y-3">
+        <h1 className="bg-red-300">App</h1>
+        <ButtonLg btns={buttongroup}></ButtonLg>
+      </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
+  purge: [
+    './dist/*.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: Object.assign(colors, {
+      primary: colors.fuchsia,
+      secondary: colors.pink,
+      lessprior: colors.warmGray
+    }),
     extend: {},
   },
   variants: {
